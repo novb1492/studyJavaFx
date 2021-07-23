be011c690678d613b119618d3a44ba18a0d96f40
novb1492 <novb1492@naver.com>
2021-07-23 14:44:48 +0900

    grid

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,9 +1,12 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
@@ -50,5 +53,25 @@ public class Ex4 extends Application{
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
+		//그리드
+		Button bu=new Button("버튼1");
+		Button bu2=new Button("버튼2");
+		Label label=new Label("label");
+		Label label2=new Label("label2");
+		
+		GridPane.setConstraints(label, 0, 1);
+		GridPane.setConstraints(label2, 0, 2);
+		
+		GridPane.setConstraints(bu,1, 1);
+		GridPane.setConstraints(bu2, 1, 2);
+		
+		gridPane.getChildren().addAll(bu,bu2,label,label2);
+		gridPane.setPadding(new Insets(20, 0, 0, 20));//상 우 하좌 top,right,botton,left
+		gridPane.setVgap(20);
+		gridPane.setHgap(40);
+		scene=new Scene(gridPane,1000,500);
+		primaryStage.setTitle("");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
